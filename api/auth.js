388e5ce795a337aa0463aac8c1109a929193dc4f
JasonCCYY <jason80124@gmail.com>
2026-05-19 01:27:50 +0800

    Delete api directory

--- api/auth.js
+++
@@ -1,5 +0,0 @@
-module.exports = function handler(req, res) {
-  if (req.method !== 'POST') return res.status(405).end();
-  const { password } = req.body;
-  res.json({ ok: password === process.env.SITE_PASSWORD });
-};
